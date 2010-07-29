37ee5bcf7af5e69bea56ee9c417ec3dd1e232a91
Gary MacDonald <garymd@yahoo-inc.com>
2010-07-29 13:24:05 -0700

    cleanup

--- external/build.rb
+++ external/build.rb
@@ -2,7 +2,6 @@
 
 require "bakery/ports/bakery"
 
-# boost is built from bakery port, bp-file is built from submodule source
 topDir = File.dirname(File.expand_path(__FILE__));
 $order = {
   :output_dir => File.join(topDir, "dist"),
@@ -13,11 +12,11 @@ $order = {
                ],
   :verbose => true,
   :use_source => {
-        "bp-file"=>File.join(topDir, "bp-file")
-   },
+    "bp-file"=>File.join(topDir, "bp-file")
+  },
   :use_recipe => {
-        "bp-file"=>File.join(topDir, "bp-file", "recipe.rb")
-   }
+    "bp-file"=>File.join(topDir, "bp-file", "recipe.rb")
+  }
 }
 
 b = Bakery.new $order
